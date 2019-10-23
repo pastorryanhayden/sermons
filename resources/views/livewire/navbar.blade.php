e99e0c151671fe864baabc7726860b130a065574
Ryan Hayden <pastorryanhayden@gmail.com>
2019-10-23 08:02:12 -0500

    added patreon button

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -7,13 +7,14 @@
                     <span class="text-lg uppercase tracking-wide inline-flex items-end -ml-2">Sermons</span>
                 </button>
             </div>
-            <div data-title="Login/Account Settings" class="flex justify-end flex-grow flex-1 text-right relative">
+            <div data-title="Login/Account Settings" class="flex justify-end flex-grow flex-1 text-right items-center relative">
                 @guest
                 <a class="no-underline hover:underline text-gray-800 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
                 @if (Route::has('register'))
                 <a class="no-underline hover:underline text-gray-800 text-sm p-3" href="/terms">{{ __('Register') }}</a>
                 @endif
                 @else
+                <a href="https://www.patreon.com/user?u=26067398" target="_blank" class="py-2 px-4 rounded-full bg-green-500 text-white mr-6 text-center">Support <span class="hidden md:inline">ChurchTools.co</span></a>
                 <button wire:click.prefetch="toggleSettings" class="flex justify-end items-end"><span class="rounded-full border bg-gray-100 text-gray-800 h-8 w-8 inline-flex items-center justify-center text-center p-4">{{ $userinitials }}</span>@component('svg.chevron-down') h-3 mb-1 text-gray-400 @endcomponent </button>
                 @if($settingsOpen)
                 <div wire:mouseleave="toggleSettings" class="absolute top-0 right-0 bg-white mt-10 shadow flex flex-col items-start justify-start w-48 text-left">
