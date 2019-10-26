e5ef3d03ca6f367a55a85e760ea97909811888c3
Ryan Hayden <pastorryanhayden@gmail.com>
2019-10-26 06:42:26 -0500

    more bugs

--- app/Http/Controllers/SermonsMediaController.php
+++ app/Http/Controllers/SermonsMediaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
 use App\Sermon;
 
 class SermonsMediaController extends Controller
--- app/Http/Controllers/SermonsTextsController.php
+++ app/Http/Controllers/SermonsTextsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Sermon;
 use App\Book;
+use Illuminate\Support\Facades\Auth;
 
 class SermonsTextsController extends Controller
 {
