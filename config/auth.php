70d19919744a8172fd8f0225d1655edab3543e4a
Ryan Hayden <pastorryanhayden@gmail.com>
2019-10-28 07:03:11 -0500

    readded password resets to main app

--- config/auth.php
+++ config/auth.php
@@ -72,7 +72,7 @@ return [
         ],
 
         // 'users' => [
-        //     'driver' => 'database',
+        //     'driver' => 'userbase',
         //     'table' => 'users',
         // ],
     ],
