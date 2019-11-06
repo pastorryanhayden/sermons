039037e81d5b1646113c5a2650e7ddde108320e7
Ryan Hayden <pastorryanhayden@gmail.com>
2019-11-06 05:55:15 -0600

    added my email to telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                'pastorryanhayden@gmail.com',
+                'ryan@rowandtable.com'
             ]);
         });
     }
